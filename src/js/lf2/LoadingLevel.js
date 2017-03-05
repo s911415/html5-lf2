4fb9ca20bb3bd547058bfeadf39b3acc81391e54
WeiChun <weichun@csie.tw>
2017-03-06 02:33:17 +0800

    remove array function

--- src/js/lf2/LoadingLevel.js
+++ src/js/lf2/LoadingLevel.js
@@ -76,20 +76,20 @@ var lf2 = (function (lf2) {
                 }).then((data) => {
                     console.log('Load data list done.');
 
-                    const objs = data.object, bgs = data.background;
+                    const objs = data.object, bgs = data.background, $=this;
 
-                    objs.forEach((o) => {
+                    objs.forEach(function(o){
                         const _o = o;
-                        this.promiseList.push(
+                        $.promiseList.push(
                             new Promise((res, rej) => {
                                 ResourceManager.loadResource(define.DATA_PATH + _o.file).then((data) => {
                                     return data.text();
                                 }).then((datText) => {
-                                    const obj = this.parseObj(_o, datText);
+                                    const obj = $.parseObj(_o, datText);
                                     if (obj instanceof GameObject) {
-                                        this.objInfo.push(obj);
+                                        $.objInfo.push(obj);
 
-                                        this.promiseList.push(
+                                        $.promiseList.push(
                                             obj.done().then(res)
                                         );
                                     } else {
