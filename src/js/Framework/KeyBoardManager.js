fdd5b0cbae7753b0256fc6a943870424262cb2a7
WeiChun <weichun@csie.tw>
2017-03-16 17:37:53 +0800

    新增按鍵白名單功能

--- src/js/Framework/KeyBoardManager.js
+++ src/js/Framework/KeyBoardManager.js
@@ -117,8 +117,33 @@ var Framework = (function (Framework) {
             _subject;
         for (let keyCode in _keyCodeToChar) _stringToKeyCode[_keyCodeToChar[keyCode]] = keyCode;
 
+        const KEY_WHITE_LIST = [
+            _stringToKeyCode['F1'],
+            _stringToKeyCode['F2'],
+            _stringToKeyCode['F3'],
+            _stringToKeyCode['F4'],
+            _stringToKeyCode['F5'],
+            _stringToKeyCode['F6'],
+            _stringToKeyCode['F7'],
+            _stringToKeyCode['F8'],
+            _stringToKeyCode['F9'],
+            _stringToKeyCode['F10'],
+            _stringToKeyCode['F11'],
+            _stringToKeyCode['F12'],
+        ];
+
+        /**
+         * @return {boolean}
+         */
+        const IN_WHITE_LIST = (e)=>{
+            return KEY_WHITE_LIST.indexOf(e.keyCode)!==-1;
+        };
+
         var keydownEvent = function (e) {
-            e.preventDefault();
+            if (IN_WHITE_LIST(e)) {
+                e.preventDefault();
+            }
+
             _keyStatus[e.keyCode] = true;
             var keyCode = _keyCodeToChar[e.which || e.keyCode], i;
             if (!Framework.Util.isUndefined(_keydownList[keyCode])) {
@@ -140,7 +165,10 @@ var Framework = (function (Framework) {
         };
 
         var keyupEvent = function (e) {
-            e.preventDefault();
+            if (IN_WHITE_LIST(e)) {
+                e.preventDefault();
+            }
+            
             _keyStatus[e.keyCode] = false;
             var keyCode = _keyCodeToChar[e.which || e.keyCode], temp = {};
             _keypressHistory.push(_keydownList[keyCode]);
