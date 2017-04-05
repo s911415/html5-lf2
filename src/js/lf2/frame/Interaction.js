d554c00671aeae344eaacda82f90bc035cab27f1
WeiChun <weichun@csie.tw>
2017-04-05 16:45:01 +0800

    fix bdy kind

--- src/js/lf2/frame/Interaction.js
+++ src/js/lf2/frame/Interaction.js
@@ -15,7 +15,7 @@ var lf2 = (function (lf2) {
          */
         constructor(content) {
             this.info = Utils.parseDataLine(content.replace(/\r?\n/g, ""));
-            this.kind = intval(this.info.get('kind'));
+            this.kind = intval(this.info.get('kind') || 0);
             this.rect = new Rectangle(
                 intval(this.info.get('w')), intval(this.info.get('h')),
                 intval(this.info.get('x')), intval(this.info.get('y'))
