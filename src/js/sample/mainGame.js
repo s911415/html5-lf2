df80b691183e9f32b3e80280e4c7fefbe912e464
WeiChun <weichun@csie.tw>
2017-05-25 19:04:25 +0800

    移除Sample

--- src/js/sample/mainGame.js
+++
@@ -1,9 +0,0 @@
-//當有要加關卡時, 可以使用addNewLevel
-//第一個被加進來的Level就是啟動點, 所以一開始遊戲就進入MyMenu
-{
-    Framework.Game.addNewLevel({menu: new Sample.MyMenu()});
-    Framework.Game.addNewLevel({level1: new Sample.MyGame()});
-
-    //讓Game開始運行
-    Framework.Game.start();
-}
