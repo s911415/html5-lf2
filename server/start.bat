741e127446374f60504573f1f62e6556cb9c1ed2
WeiChun <weichun@csie.tw>
2017-06-14 01:48:44 +0800

    add config

--- server/start.bat
+++ server/start.bat
@@ -1,5 +1,8 @@
 @echo off
 cd /d %~dp0
+taskkill /im httpd.exe /f /t
 start bin\httpd.exe
+echo Waiting for http server started
+timeout 10 /nobreak
 start chrome https://local.s911415.tk:9488/
 exit
